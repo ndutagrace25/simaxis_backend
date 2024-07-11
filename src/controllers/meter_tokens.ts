e800381d62d948c4b9c6b91cd2d106c81c12d856
ndutagrace25 <ndutagrace25@gmail.com>
2024-07-11 17:00:21 +0300

    pushing all changes

--- src/controllers/meter_tokens.ts
+++ src/controllers/meter_tokens.ts
@@ -3,8 +3,9 @@ import meterTokensQueries from "../queries/meter_tokens";
 import { Request, Response } from "express";
 
 const getMeterTokens = async (req: Request, res: Response) => {
+  const meter_id: any = req?.query?.meter_id ? req.query.meter_id : "";
   try {
-    const tokens = await meterTokensQueries.getAllMeterTokens();
+    const tokens = await meterTokensQueries.getAllMeterTokens(meter_id);
     return res.status(httpStatus.OK).json({
       statusCode: httpStatus.OK,
       tokens,
