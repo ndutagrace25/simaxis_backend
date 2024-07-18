9361227f395fcae9e1052715ec6afbea70a8abcd
ndutagrace25 <ndutagrace25@gmail.com>
2024-07-18 12:47:02 +0300

    dd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,14 +4,13 @@ module.exports = {
         name: "simaxis",
         script: "./node_modules/.bin/ts-node",
         args: "-r tsconfig-paths/register src/index.ts",
-        interpreter: "./node_modules/.bin/ts-node",
         watch: true,
         ignore_watch: ["node_modules"],
         env: {
           NODE_ENV: "development"
         },
         env_production: {
-          NODE_ENV: "development"
+          NODE_ENV: "production"
         }
       }
     ]
