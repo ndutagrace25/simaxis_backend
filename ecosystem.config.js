7f484f9fb8c3511647cb0d9c61e8b20923e0bbd9
ndutagrace25 <ndutagrace25@gmail.com>
2024-07-18 12:58:33 +0300

    pushing all chnages

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,10 @@
 module.exports = {
-    apps: [
-      {
-        name: "simaxis",
-        script: "./node_modules/.bin/ts-node",
-        args: "-r tsconfig-paths/register src/index.ts",
-        watch: true,
-        ignore_watch: ["node_modules"],
-        env: {
-          NODE_ENV: "development"
-        },
-        env_production: {
-          NODE_ENV: "production"
-        }
+    apps: [{
+      name: "simaxis",
+      script: "./dist/index.js",
+      watch: true,
+      env: {
+        "NODE_ENV": "development",
       }
-    ]
-  };
-  
+    }]
+  }
